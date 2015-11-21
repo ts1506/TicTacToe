bc812401900856f8dd92e96924f56f9dfb06f4c5
Shaunak Guharay <guharayshaunak75@gmail.com>
2015-11-22 04:49:17 +0530

    Initial push

---
+++ TicTacToe/TicTacToe_MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe_MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.R1C1 = new System.Windows.Forms.Button();
+            this.R1C2 = new System.Windows.Forms.Button();
+            this.R1C3 = new System.Windows.Forms.Button();
+            this.R2C1 = new System.Windows.Forms.Button();
+            this.R2C2 = new System.Windows.Forms.Button();
+            this.R2C3 = new System.Windows.Forms.Button();
+            this.R3C1 = new System.Windows.Forms.Button();
+            this.R3C2 = new System.Windows.Forms.Button();
+            this.R3C3 = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // R1C1
+            // 
+            this.R1C1.Location = new System.Drawing.Point(12, 12);
+            this.R1C1.Name = "R1C1";
+            this.R1C1.Size = new System.Drawing.Size(65, 65);
+            this.R1C1.TabIndex = 0;
+            this.R1C1.UseVisualStyleBackColor = true;
+            this.R1C1.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R1C2
+            // 
+            this.R1C2.Location = new System.Drawing.Point(83, 12);
+            this.R1C2.Name = "R1C2";
+            this.R1C2.Size = new System.Drawing.Size(65, 65);
+            this.R1C2.TabIndex = 1;
+            this.R1C2.UseVisualStyleBackColor = true;
+            this.R1C2.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R1C3
+            // 
+            this.R1C3.Location = new System.Drawing.Point(156, 12);
+            this.R1C3.Name = "R1C3";
+            this.R1C3.Size = new System.Drawing.Size(65, 65);
+            this.R1C3.TabIndex = 2;
+            this.R1C3.UseVisualStyleBackColor = true;
+            this.R1C3.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R2C1
+            // 
+            this.R2C1.Location = new System.Drawing.Point(12, 86);
+            this.R2C1.Name = "R2C1";
+            this.R2C1.Size = new System.Drawing.Size(65, 65);
+            this.R2C1.TabIndex = 3;
+            this.R2C1.UseVisualStyleBackColor = true;
+            this.R2C1.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R2C2
+            // 
+            this.R2C2.Location = new System.Drawing.Point(83, 86);
+            this.R2C2.Name = "R2C2";
+            this.R2C2.Size = new System.Drawing.Size(65, 65);
+            this.R2C2.TabIndex = 4;
+            this.R2C2.UseVisualStyleBackColor = true;
+            this.R2C2.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R2C3
+            // 
+            this.R2C3.Location = new System.Drawing.Point(154, 86);
+            this.R2C3.Name = "R2C3";
+            this.R2C3.Size = new System.Drawing.Size(67, 65);
+            this.R2C3.TabIndex = 5;
+            this.R2C3.UseVisualStyleBackColor = true;
+            this.R2C3.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R3C1
+            // 
+            this.R3C1.Location = new System.Drawing.Point(12, 157);
+            this.R3C1.Name = "R3C1";
+            this.R3C1.Size = new System.Drawing.Size(65, 65);
+            this.R3C1.TabIndex = 6;
+            this.R3C1.UseVisualStyleBackColor = true;
+            this.R3C1.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R3C2
+            // 
+            this.R3C2.Location = new System.Drawing.Point(83, 157);
+            this.R3C2.Name = "R3C2";
+            this.R3C2.Size = new System.Drawing.Size(65, 65);
+            this.R3C2.TabIndex = 7;
+            this.R3C2.UseVisualStyleBackColor = true;
+            this.R3C2.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // R3C3
+            // 
+            this.R3C3.Location = new System.Drawing.Point(154, 157);
+            this.R3C3.Name = "R3C3";
+            this.R3C3.Size = new System.Drawing.Size(67, 65);
+            this.R3C3.TabIndex = 8;
+            this.R3C3.UseVisualStyleBackColor = true;
+            this.R3C3.Click += new System.EventHandler(this.toggle_pos);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(122, 232);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(99, 23);
+            this.Exit.TabIndex = 9;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.quit_app);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 267);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.R3C3);
+            this.Controls.Add(this.R3C2);
+            this.Controls.Add(this.R3C1);
+            this.Controls.Add(this.R2C3);
+            this.Controls.Add(this.R2C2);
+            this.Controls.Add(this.R2C1);
+            this.Controls.Add(this.R1C3);
+            this.Controls.Add(this.R1C2);
+            this.Controls.Add(this.R1C1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button R1C1;
+        private System.Windows.Forms.Button R1C2;
+        private System.Windows.Forms.Button R1C3;
+        private System.Windows.Forms.Button R2C1;
+        private System.Windows.Forms.Button R2C2;
+        private System.Windows.Forms.Button R2C3;
+        private System.Windows.Forms.Button R3C1;
+        private System.Windows.Forms.Button R3C2;
+        private System.Windows.Forms.Button R3C3;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ TicTacToe/TicTacToe_MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe {
+    public partial class TicTacToe_MainForm : Form {
+        // True = 1st person False = 2nd person
+        bool check_turn = true;
+        int count_turn = 0;
+
+        public TicTacToe_MainForm() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void toggle_pos(object sender, EventArgs e) {
+            Button a = (Button)sender;
+            if (check_turn == true)
+                a.Text = "X";
+            else
+                a.Text = "O";
+
+            // Switch bool, disable button and count turns
+            check_turn = !check_turn;
+            a.Enabled = false;
+            count_turn++;
+
+            // Check if out of turns
+            if (count_turn == 9)
+                MessageBox.Show("Its a draw");
+            else
+                // Check if we have a winner
+                win_check();
+
+        }
+
+        private void win_check()  {
+            bool winner = false;
+
+            if ((R1C1.Text == R1C2.Text) && (R1C2.Text == R1C3.Text) && !R1C1.Enabled)
+                winner = true;
+            if ((R2C1.Text == R2C2.Text) && (R2C2.Text == R2C3.Text) && !R2C1.Enabled)
+                winner = true;
+            if ((R3C1.Text == R3C2.Text) && (R3C2.Text == R3C3.Text) && !R3C1.Enabled)
+                winner = true;
+            if ((R1C1.Text == R2C1.Text) && (R2C1.Text == R3C1.Text) && !R1C1.Enabled)
+                winner = true;
+            if ((R1C2.Text == R2C2.Text) && (R2C2.Text == R3C2.Text) && !R1C2.Enabled)
+                winner = true;
+            if ((R1C3.Text == R2C3.Text) && (R2C3.Text == R3C3.Text) && !R1C3.Enabled)
+                winner = true;
+            if ((R1C1.Text == R2C2.Text) && (R2C2.Text == R3C3.Text) && !R1C1.Enabled)
+                winner = true;
+            if ((R1C3.Text == R2C2.Text) && (R2C2.Text == R1C3.Text) && !R1C3.Enabled)
+                winner = true;
+
+            if(winner)
+            {
+                if (check_turn)
+                    MessageBox.Show("2nd Player won!!");
+                else
+                    MessageBox.Show("1st Player won!!");
+            }
+        }
+
+        private void quit_app(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
